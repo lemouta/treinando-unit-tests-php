1e7e03a122efb75b22d6c8ed13dd5612c30dbaf1
Leandro Mouta <leandro@mouta.net>
2020-07-04 21:52:52 -0300

    IPFizzBuzz service tests

--- tests/IPFizzBuzzTest.php
+++ tests/IPFizzBuzzTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Exception\InvalidIP4Format;
 use PHPUnit\Framework\TestCase;
 
 class IPFizzBuzzTest extends TestCase
@@ -16,8 +17,50 @@ class IPFizzBuzzTest extends TestCase
     /**
      * @test
      */
+    public function shouldThrowExceptionForIpWithoutFourParts()
+    {
+        $this->expectException(InvalidIP4Format::class);
+        $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionForIpWithNanLastPart()
+    {
+        $this->expectException(InvalidIP4Format::class);
+        $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.sagan");
+    }
+
+    /**
+     * @test
+     */
     public function shouldReturnFizz()
     {
         $this->assertEquals("Fizz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.3"));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnBuzz()
+    {
+        $this->assertEquals("Buzz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.5"));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnLastNumber()
+    {
+        $this->assertEquals("7", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.7"));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnFizzBuzz()
+    {
+        $this->assertEquals("FizzBuzz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.15"));
     }
 }
